87efe45273151b5d298ba930ae51faa9ba63b172
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2023-01-04 15:41:26 -0500

    tests: add evalables to the dummy mc

--- test/test_mc.jl
+++ test/test_mc.jl
@@ -9,13 +9,21 @@ LoadLeveller.sweep!(mc::TestMC, ctx::LoadLeveller.MCContext) = nothing
 
 function LoadLeveller.measure!(mc::TestMC, ctx::LoadLeveller.MCContext)
     LoadLeveller.measure!(ctx, :test, ctx.sweeps)
+    LoadLeveller.measure!(ctx, :test2, ctx.sweeps^2)
+
+    return nothing
 end
 
 LoadLeveller.write_checkpoint(mc::TestMC, out::HDF5.Group) = nothing
 LoadLeveller.read_checkpoint!(mc::TestMC, in::HDF5.Group) = nothing
 
-LoadLeveller.register_evaluables(
+function LoadLeveller.register_evaluables(
     ::Type{TestMC},
     eval::LoadLeveller.Evaluator,
     params::AbstractDict,
-) = nothing
+)
+    evaluate!((x, y) -> y - x^2, eval, :test4, [:test, :test2])
+    evaluate!(x -> x^2, eval, :test5, [:test])
+
+    return nothing
+end
