dc754d31c3b3cc49ce9af370e2358b6aa976fc42
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2023-01-04 15:37:28 -0500

    util: remove unused function create_absent_dataset

--- src/util.jl
+++ src/util.jl
@@ -13,20 +13,6 @@ function create_absent_group(
     end
 end
 
-"""Helper to create a dataset inside a HDF5 node but only if it does not already exist."""
-function create_absent_dataset(
-    g::Union{HDF5.File,HDF5.Group},
-    name::AbstractString,
-    args...;
-    kwargs...,
-)::HDF5.Dataset
-    return if haskey(g, name)
-        g[name]
-    else
-        create_dataset(g, name, args...; kwargs...)
-    end
-end
-
 """Parse a duration of the format `[[hours:]minutes]:seconds`."""
 function parse_duration(duration::AbstractString)::Dates.CompoundPeriod
     m = match(r"^(((?<hours>\d+):)?(?<minutes>\d+):)?(?<seconds>\d+)$", duration)
