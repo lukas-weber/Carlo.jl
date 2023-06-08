9221395774c1ee2120fa5198379935f3b4bf9eec
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2023-06-08 16:52:02 -0400

    version: remove the hashes for now. they are too slow...

--- src/version.jl
+++ src/version.jl
@@ -1,18 +1,3 @@
-
-function get_hash_or_missing(mod)
-    hash::Union{Missing,String} = missing
-    try
-        hash = PackageStates.state(mod).directory_tree_hash
-    catch e
-        if e isa ErrorException
-            hash = missing
-        else
-            throw(e)
-        end
-    end
-    return hash
-end
-
 function pkgversion_or_missing(mod)
     if isdefined(Base, :pkgversion)
         return string(pkgversion)
@@ -23,22 +8,18 @@ end
 """
     Version
 
-The version information for both `LoadLeveller` and the parent module of the `AbstractMC` implementation that is currently used. It contains both the version of the Julia packages and their directory tree hashes. The hashes are content hashes of the package directories, as provided by the `PackageStates` package, at the time of execution.
+The version information for both `LoadLeveller` and the parent module of the `AbstractMC` implementation that is currently used.
 """
 struct Version
     loadleveller_version::Union{Missing,String}
-    loadleveller_hash::Union{Missing,String}
     mc_package::String
     mc_version::Union{Missing,String}
-    mc_hash::Union{Missing,String}
 
     function Version(mc::Type{<:AbstractMC})
         return new(
             pkgversion_or_missing(@__MODULE__),
-            get_hash_or_missing(@__MODULE__),
             string(parentmodule(mc)),
             pkgversion_or_missing(parentmodule(mc)),
-            get_hash_or_missing(parentmodule(mc)),
         )
     end
 end
