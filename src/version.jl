6218519779db26f99fade9f85b85b174f25b0980
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2023-06-08 15:22:16 -0400

    make the version mechanism more robust in case hashes are unavailable

--- src/version.jl
+++ src/version.jl
@@ -1,29 +1,43 @@
+
+function get_hash_or_missing(mod)
+    hash::Union{Missing,String} = missing
+    try
+        hash = PackageStates.state(mod).directory_tree_hash
+    catch e
+        if e isa ErrorException
+            hash = missing
+        end
+        throw(e)
+    end
+    return hash
+end
+
 """
     Version
 
-The version information for both LoadLeveller and the parent module of the AbstractMC implementation that is currently used. It contains both the version of the Julia packages and their directory tree hashes. The hashes are content hashes of the package directories, as provided by the PackageStates package, at the time of execution.
+The version information for both `LoadLeveller` and the parent module of the `AbstractMC` implementation that is currently used. It contains both the version of the Julia packages and their directory tree hashes. The hashes are content hashes of the package directories, as provided by the `PackageStates` package, at the time of execution.
 """
 struct Version
     loadleveller_version::String
-    loadleveller_hash::String
+    loadleveller_hash::Union{Missing,String}
     mc_package::String
     mc_version::String
-    mc_hash::String
+    mc_hash::Union{Missing,String}
 
     function Version(mc::Type{<:AbstractMC})
         return new(
             string(pkgversion(@__MODULE__)),
-            PackageStates.state(@__MODULE__).directory_tree_hash,
+            get_hash_or_missing(@__MODULE__),
             string(parentmodule(mc)),
             string(pkgversion(parentmodule(mc))),
-            PackageStates.state(parentmodule(mc)).directory_tree_hash,
+            get_hash_or_missing(parentmodule(mc)),
         )
     end
 end
 
 function write_hdf5(version::Version, group::HDF5.Group)
     for (field, value) in to_dict(version)
-        group[field] = value
+        group[field] = string(value)
     end
 end
 
