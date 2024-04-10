7bdfe9e6d146647c6c67f553838273169afb5718
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2024-04-10 16:37:43 -0400

    test: clean up temporary directory sooner

--- test/test_cli.jl
+++ test/test_cli.jl
@@ -21,46 +21,47 @@ function test_binning_properties(result_file)
 end
 
 @testset "CLI" begin
-    tmpdir = mktempdir()
-    dummy_jobfile = "dummy_jobfile.jl"
+    mktempdir() do tmpdir
+        dummy_jobfile = "dummy_jobfile.jl"
 
-    run_cmd(cmd; quiet = false) = run(
-        pipeline(
-            `$(Base.julia_cmd()) $dummy_jobfile $tmpdir $cmd`,
-            stderr = quiet ? devnull : stderr,
-        ),
-    )
+        run_cmd(cmd; quiet = false) = run(
+            pipeline(
+                `$(Base.julia_cmd()) $dummy_jobfile $tmpdir $cmd`,
+                stderr = quiet ? devnull : stderr,
+            ),
+        )
 
-    @test_throws ProcessFailedException run_cmd("status"; quiet = true)
-    @test_throws ProcessFailedException run_cmd("merge"; quiet = true)
-    run_cmd("delete")
-    @test !isfile(tmpdir * "/test.results.json")
+        @test_throws ProcessFailedException run_cmd("status"; quiet = true)
+        @test_throws ProcessFailedException run_cmd("merge"; quiet = true)
+        run_cmd("delete")
+        @test !isfile(tmpdir * "/test.results.json")
 
-    run_cmd(`run`)
+        run_cmd(`run`)
 
-    run_cmd("status")
-    run_cmd("merge")
-    @test isfile(tmpdir * "/test.results.json")
+        run_cmd("status")
+        run_cmd("merge")
+        @test isfile(tmpdir * "/test.results.json")
 
-    test_binning_properties(tmpdir * "/test.results.json")
-    run_cmd("delete")
-    @test !isfile(tmpdir * "/test.results.json")
-    @test !isdir(tmpdir * "/test")
+        test_binning_properties(tmpdir * "/test.results.json")
+        run_cmd("delete")
+        @test !isfile(tmpdir * "/test.results.json")
+        @test !isdir(tmpdir * "/test")
 
-    tm = TaskMaker()
-    tm.sweeps = 100
-    tm.thermalization = 100
-    tm.binsize = 10
-    task(tm)
+        tm = TaskMaker()
+        tm.sweeps = 100
+        tm.thermalization = 100
+        tm.binsize = 10
+        task(tm)
 
-    job = JobInfo(
-        tmpdir * "/test",
-        TestMC;
-        tasks = make_tasks(tm),
-        checkpoint_time = "00:05",
-        run_time = "00:10",
-    )
+        job = JobInfo(
+            tmpdir * "/test",
+            TestMC;
+            tasks = make_tasks(tm),
+            checkpoint_time = "00:05",
+            run_time = "00:10",
+        )
 
-    JobTools.create_job_directory(job)
-    run_cmd("status")
+        JobTools.create_job_directory(job)
+        run_cmd("status")
+    end
 end
