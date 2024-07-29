e25617dd41e579706804dbb5e9dd24beec813da3
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2024-07-29 16:19:56 -0400

    avoid exposing test types through precompilation
    The precompilation workload used to use the dummy `TestMC` to
    run a bunch of Carlo functions. However,
    
    1. This leaked the types of test_mc.jl into the Carlo namespace.
    2. The precompilation would cover many methods that specialize on `TestMC`,
       which are useless in practice.
    
    This commit removes most of the precompilation workflow and the dependence
    on test_mc.jl.

--- src/precompile.jl
+++ src/precompile.jl
@@ -2,7 +2,6 @@ using PrecompileTools
 using ..JobTools
 
 @setup_workload begin
-    include("../test/test_mc.jl")
     @compile_workload begin
         tm = TaskMaker()
         tm.thermalization = 10
@@ -11,23 +10,5 @@ using ..JobTools
         tm.test = [1]
 
         task(tm, Lx = 2, T = 1)
-        mktempdir() do dir
-
-            job = JobInfo(
-                dir * "/precompile_test",
-                TestMC;
-                tasks = make_tasks(tm),
-                checkpoint_time = "30:00",
-                run_time = "24:00:00",
-            )
-
-            redirect_stdio(stdout = devnull, stderr = devnull) do
-                start(job, ["run", "-s"])
-                # start(job, ["run"])
-                start(job, ["status"])
-                start(job, ["merge"])
-                start(job, ["delete"])
-            end
-        end
     end
 end
