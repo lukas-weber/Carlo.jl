c33f72ebef88fe7afda3f37fd0f5e7f78ef054e7
LeoXia <114814844+hz-xiaxz@users.noreply.github.com>
2026-04-18 01:31:32 +0800

    Change default Parellel Tempering Tag (#32)
    * change to 2^19-1 to align with intel MPI tag_ub
    
    * update
    
    * change to 32767, leaving some vacany for the upper bound

--- src/parallel_tempering.jl
+++ src/parallel_tempering.jl
@@ -1,6 +1,6 @@
 # MPI tags
-const PT_WEIGHT_MSG = 4573792
-const PT_SWITCH_MSG = 4573793
+const PT_WEIGHT_MSG = 32732
+const PT_SWITCH_MSG = 32733 
 
 struct ParallelMeasurements
     queue::Vector{Tuple{Symbol,Any}}
